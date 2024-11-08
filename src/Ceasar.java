a7ad5642c4e90eefb355bb6e02673c93121d7af8
Tom <tom@mvjt.de>
2024-11-08 10:39:09 +0100

    classes

---
+++ src/Ceasar.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Ceasar {
+
+    private String gt;
+    private String kt;
+    private int schluessel;
+
+    public class Ceasar();
+    public void verschluesseln();
+
+    private char getChar(int pWert){
+        return (char)pWert;
+    }
+    private int getAscii(char)
+
+
+
+
+
+
+
+
+}
