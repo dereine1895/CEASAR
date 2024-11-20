99cdb7897567e256fb749765baeff6a83e7a2462
Tom <tom@mvjt.de>
2024-11-20 11:29:07 +0100

    verwaltung "fertig"

--- src/Verwaltung.java
+++ src/Verwaltung.java
@@ -9,51 +9,51 @@ public class Verwaltung{
     }
     public Verwaltung() {
         scanner = new Scanner(System.in);
+        ceasar = new Ceasar();
         while (true) {
             System.out.println("== HAUPTMENÜ ==");
             System.out.println("[1]Ceasar");
             System.out.println("[2]Vigenere");
             int option = scanner.nextInt();
-            System.out.println("[1] verschluesseln");
-            System.out.println("[2] entschluesseln");
-            int option = scanner.nextInt();
-            scanner.nextLine(); // nextInt scannt keine neue Zeile. Ohne diese Anweisung würde das nächste gewollte nextLine nicht funktionieren.
             if (option == 1) {
-                System.out.println("Gib das zu verschlüsselnde Wort an:");
-                String zeichenkette = scanner.nextLine();
-                System.out.println("Gib den Schlüssel an:");
-                String schluesselString = scanner.nextLine();
-                int schluessel = Integer.parseInt(schluesselString);
-                ceasar= new Ceasar();
-                this.ceasar.setkt(zeichenkette);
-                this.ceasar.setS(schluessel);
-                System.out.println("=======Klartext=======");
-                System.out.println(this.ceasar.getkt());
-                this.ceasar.verschluesseln();
-                System.out.println("=======Geheimtext=======");
-                System.out.println(this.ceasar.getgt());
+                System.out.println("[1] verschluesseln");
+                System.out.println("[2] entschluesseln");
+                scanner.nextInt();
+                 // nextInt scannt keine neue Zeile. Ohne diese Anweisung würde das nächste gewollte nextLine nicht funktionieren.
+                if (option == 1) {
+                    System.out.println("Gib das zu verschlüsselnde Wort an:");
+                    String zeichenkette = scanner.nextLine();
+                    System.out.println("Gib den Schlüssel an:");
+                    String schluesselString = scanner.nextLine();
+                    int schluessel = Integer.parseInt(schluesselString);
+                    this.ceasar.setkt(zeichenkette);
+                    this.ceasar.setS(schluessel);
+                    System.out.println("=======Klartext=======");
+                    System.out.println(this.ceasar.getkt());
+                    this.ceasar.verschluesseln();
+                    System.out.println("=======Geheimtext=======");
+                    System.out.println(this.ceasar.getgt());
 
-            } else if (option == 2) {
-                System.out.println("Gib das zu entschlüsselnde Wort an:");
-                String zeichenkette = scanner.nextLine();
-                System.out.println("Gib den Schlüssel an:");
-                String schluesselString = scanner.nextLine();
-                int schluessel = Integer.parseInt(schluesselString);
-                ceasar= new Ceasar();
-                this.ceasar.setgt(zeichenkette);
-                this.ceasar.setS(schluessel);
-                System.out.println("=======Geheimtext=======");
-                System.out.println(zeichenkette);
-                this.ceasar.entschluesseln();
-                System.out.println("=======Klartext=======");
-                System.out.println(this.ceasar.getkt());
+                } else if (option == 2) {
+                    System.out.println("Gib das zu entschlüsselnde Wort an:");
+                    String zeichenkette = scanner.nextLine();
+                    System.out.println("Gib den Schlüssel an:");
+                    String schluesselString = scanner.nextLine();
+                    int schluessel = Integer.parseInt(schluesselString);
+                    this.ceasar.setgt(zeichenkette);
+                    this.ceasar.setS(schluessel);
+                    System.out.println("=======Geheimtext=======");
+                    System.out.println(zeichenkette);
+                    this.ceasar.entschluesseln();
+                    System.out.println("=======Klartext=======");
+                    System.out.println(this.ceasar.getkt());
 
 
+                } else if (option == 0) {
+                    break;
+                }
+                System.out.println();
             }
-            else if (option == 0) {
-                break;
-            }
-            System.out.println();
         }
     }
 
