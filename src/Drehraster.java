0b7817d578fe1c5095c29258999889e5c3367266
dereine1895 <tom@mvjt.de>
2024-12-17 18:28:06 +0100

    drehraster funktioniert (más o menos)

--- src/Drehraster.java
+++ src/Drehraster.java
@@ -3,17 +3,28 @@ public class Drehraster extends Kryptomat {
     private int s;
     public Drehraster(){
         kt="";
+        gt="";
         s = 0;
     }
 
-    public void verschluesseln(){
-        for (int i = 0;i < kt.length(); i++){
-            for(int j = 0; j < s;j+= s){
-                gt = gt + kt.charAt(j);
+    public void verschluesseln() {
+        // Ein Array zum Speichern des verschlüsselten Textes, mit der gleichen Länge wie der Klartext
+        char[] gtArray = new char[kt.length()];
 
-            }
+        // Durchlaufe alle Zeichen des Klartexts
+        for (int i = 0; i < kt.length(); i++) {
+            // Berechne die neue Position, die durch den Schlüssel bestimmt wird
+            int verschiebung = (i + s) % kt.length();
+
+            // Das Zeichen an der berechneten Position im Array speichern
+            gtArray[verschiebung] = kt.charAt(i);
         }
+
+        // Den verschlüsselten Text (in Form eines Strings) in der geerbten gt-Variable speichern
+        gt = new String(gtArray);
     }
+
+
     public void entschluesseln(){
 
 
